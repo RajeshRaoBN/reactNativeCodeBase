cc294a71f260ebd1943bc2b391540488d860ce18
Rajesh Narayana Rao <mailrbn@gmail.com>
2025-06-25 16:49:24 +1000

    Fifth Commit

---
+++ lesson01/constants/MenuImages.js
@@ -0,0 +1,23 @@
+import americano from "@/assets/images/menu/Americano.jpeg"
+import espresso from "@/assets/images/menu/Espresso.jpg"
+import latte from "@/assets/images/menu/Latte.jpg"
+import cappuccino from "@/assets/images/menu/cappucino.webp"
+import macchiato from "@/assets/images/menu/Macchiato.webp"
+import flatwhite from "@/assets/images/menu/flat-white.jpg"
+import cortado from "@/assets/images/menu/Cortado.webp"
+import mocha from "@/assets/images/menu/Mocha.jpg"
+import coldbrew from "@/assets/images/menu/Iced-Coffee.jpg"
+import filterbrew from "@/assets/images/menu/Filter_Coffee.webp"
+
+export default [
+    espresso,
+    americano,
+    latte,
+    cappuccino,
+    mocha,
+    macchiato,
+    flatwhite,
+    cortado,
+    filterbrew,
+    coldbrew
+]
---
+++ lesson01/constants/MenuItems.js
@@ -0,0 +1,52 @@
+export const MENU_ITEMS = [
+    {
+        "id": 1,
+        "title": "Espresso",
+        "description": "Strong, concentrated coffee."
+    },
+    {
+        "id": 2,
+        "title": "Americano",
+        "description": "SEspresso diluted with hot water."
+    },
+    {
+        "id": 3,
+        "title": "Latte",
+        "description": "Espresso with steamed milk and foam."
+    },
+    {
+        "id": 4,
+        "title": "Cappuccino",
+        "description": "Equal parts espresso, milk and foam."
+    },
+    {
+        "id": 5,
+        "title": "Mocha",
+        "description": "A latte with chocolate syrup or powder."
+    },
+    {
+        "id": 6,
+        "title": "Macchiato",
+        "description": "Espresso marked with a dollop of foamed milk."
+    },
+    {
+        "id": 7,
+        "title": "Flat White",
+        "description": "Espresso with steamed milk, similar to a latte but with less foam."
+    },
+    {
+        "id": 8,
+        "title": "Cortado",
+        "description": "Espresso cut with a small amount of warm milk."
+    },
+    {
+        "id": 9,
+        "title": "Filter Coffee",
+        "description": "Various methods of brewing coffee using a filter, like drip or pour-over."
+    },
+    {
+        "id": 10,
+        "title": "Iced Coffee",
+        "description": "Any coffee served over ice, often with milk and sweeteners."
+    }
+]
