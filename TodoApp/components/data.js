7a9ac3bf77ec7a3c5cf60690dfc03d87f3600aa7
Rajesh Narayana Rao <mailrbn@gmail.com>
2025-06-27 12:33:47 +1000

    Seventh Commit

---
+++ TodoApp/components/data.js
@@ -0,0 +1,34 @@
+export default function Data() {
+    const todoData = [
+        {
+            id: 1,
+            title: "Todo Item 1",
+            completed: false,
+        },
+        {
+            id: 2,
+            title: "Todo Item 2",
+            completed: false,
+        },
+        {
+            id: 3,
+            title: "Todo Item 3",
+            completed: false,
+        },
+        {
+            id: 4,
+            title: "Todo Item 4",
+            completed: false,
+        },
+        {
+            id: 5,
+            title: "Todo Item 5",
+            completed: false,
+        },
+        {
+            id: 6,
+            title: "Todo Item 6",
+            completed: false,
+        },
+    ]
+}
